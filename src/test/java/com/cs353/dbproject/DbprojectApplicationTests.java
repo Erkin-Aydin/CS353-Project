399e399a5dc15ab08cc2e28ee04366ed26d2830f
Ahmet Sahin <sahmet@ug.bilkent.edu.tr>
2023-05-09 22:27:54 +0300

    Initial commit

---
+++ src/test/java/com/cs353/dbproject/DbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs353.dbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
